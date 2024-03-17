802bc9d2814ede290f012684dc6e0d3fa1afb517
Nick Gupta <52716575+itsmenick212@users.noreply.github.com>
2024-03-17 16:56:53 -0700

    Add files via upload

---
+++ langchain_biomistral_ollama.py
@@ -0,0 +1,44 @@
+from langchain_community.llms import Ollama
+from langchain.prompts import ChatPromptTemplate
+from langchain.schema import StrOutputParser
+from langchain.schema.runnable import Runnable
+from langchain.schema.runnable.config import RunnableConfig
+
+import chainlit as cl
+
+
+@cl.on_chat_start
+async def on_chat_start():
+    
+    # Sending an image with the local file path
+    elements = [
+    cl.Image(name="image1", display="inline", path="dr-martin.tiff")
+    ]
+    await cl.Message(content="Hi, I am Dr. Martin AI created by Ben Litvin. How can I help you?", elements=elements).send()
+    model = Ollama(model="biomistral")
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "You're a very knowledgeable AI Doctor trained by licensed doctors, who provides accurate, eloquent, and precise answers about health issue questions. You ask follow up questions to gather all the information needed to diagnose a pressing health problem correctly.",
+            ),
+            ("human", "{question}"),
+        ]
+    )
+    runnable = prompt | model | StrOutputParser()
+    cl.user_session.set("runnable", runnable)
+
+
+@cl.on_message
+async def on_message(message: cl.Message):
+    runnable = cl.user_session.get("runnable")  # type: Runnable
+
+    msg = cl.Message(content="")
+
+    async for chunk in runnable.astream(
+        {"question": message.content},
+        config=RunnableConfig(callbacks=[cl.LangchainCallbackHandler()]),
+    ):
+        await msg.stream_token(chunk)
+
+    await msg.send()
